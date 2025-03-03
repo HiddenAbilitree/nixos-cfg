f3a4eb96adb500e5686ff7a18caa0192eb6bb516
Eric Zhang <me@ericzhang.dev>
2025-03-03 13:38:43 -0500

    :arrow_up:

--- home-modules/desktop/games/honkers.nix
+++ home-modules/desktop/games/honkers.nix
@@ -5,7 +5,7 @@
 }: let
   aagl-gtk-on-nix = import (builtins.fetchTarball {
     url = "https://github.com/ezKEa/aagl-gtk-on-nix/archive/main.tar.gz";
-    sha256 = "0v59frhfnyy7pbmbv7bdzssdp554bjsgmmm4dw31p5askysmlvib";
+    sha256 = "0f59radafvzdfn3ar1y6glx9ixc9hbvysaalsp492ixp8ihpkbxv";
   });
 in
   lib.mkIf config.desktop.games.honkers.enable {
