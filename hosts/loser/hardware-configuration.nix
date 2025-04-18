c2655f85ad1a92525b99a5fd75ea174422cab69b
Eric Zhang <me@ericzhang.dev>
2025-04-18 03:27:58 -0400

    :sparkles: :arrow_up:

--- hosts/loser/hardware-configuration.nix
+++ hosts/loser/hardware-configuration.nix
@@ -10,7 +10,13 @@
 
   boot = {
     initrd = {
-      availableKernelModules = ["nvme" "xhci_pci" "thunderbolt" "usb_storage" "sd_mod"];
+      availableKernelModules = [
+        "nvme"
+        "xhci_pci"
+        "thunderbolt"
+        "usb_storage"
+        "sd_mod"
+      ];
       kernelModules = [];
     };
     kernelModules = ["kvm-amd"];
