b3ba2d65092ede5f63c7d2c4f1aa5981fdc391f4
Eric Zhang <me@ericzhang.dev>
2025-08-05 00:34:05 -0400

    :sparkles:

--- nixos-modules/virtualization/docker.nix
+++ nixos-modules/virtualization/docker.nix
@@ -1,5 +1,7 @@
 {config, ...}: {
   virtualisation.docker = {
     inherit (config.virtualization.docker) enable;
+    enableNvidia = true;
   };
+  hardware.nvidia-container-toolkit = {inherit (config.virtualisation.docker) enable;};
 }
